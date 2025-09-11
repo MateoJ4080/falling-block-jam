0a592e8baddf99c198658996a36d9f1c128f50af
Mate93934 <mateodjm@gmail.com>
2025-09-11 00:10:03 -0300

    feat: add piece rotation with new input system

--- Assets/Scripts/Tetromino.cs
+++ Assets/Scripts/Tetromino.cs
@@ -2,5 +2,28 @@ using UnityEngine;
 
 public class Tetromino : MonoBehaviour
 {
+    private PlayerControls controls;
 
+    void Awake()
+    {
+        controls = new PlayerControls();
+
+        controls.Piece.RotateRight.performed += ctx => Rotate(-90);
+        controls.Piece.RotateLeft.performed += ctx => Rotate(90);
+    }
+
+    void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    void OnDisable()
+    {
+        controls.Disable();
+    }
+
+    public void Rotate(int angle)
+    {
+        transform.Rotate(0, 0, angle);
+    }
 }
